074d1417ca974953c252894c60ca59b81a275f81
JowenGonzales <gonzalesmj0942@gmail.com>
2023-03-11 21:06:58 +0800

    development update

---
+++ Java/RandomProjects/src/literals/Literals.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package literals;
+
+/**
+ *
+ * @author JOWEN
+ */
+public class Literals {
+    int num1 = 123456;
+    // You can seperate integer with underscore, 
+    // you could also check if they are the same using if statement
+    int num2 = 123_456; 
+    
+    // Here are more examples of these Literals
+    byte byte1 = 1_2_3;
+    short short1 = 2_106;
+    int num3 = 999_999_999;
+    long long1 = 12345_678_9012_3456L;
+    float float1 = 3.14F;
+    double double1 = 3.14_15_19_65_35;
+    
+    // To be honest i would not recomment it, because it destroys readability in a variable
+    // Not that it is hard to read, but most programmers would not be used tho this type of declaration
+    // Another thing is that it was very uneffcient to declare those. that's all (:
+    
+}
