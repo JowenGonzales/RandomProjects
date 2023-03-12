1dc5432c421d1e700cc8f571dfb8eb5caf408d95
JowenGonzales <gonzalesmj0942@gmail.com>
2023-03-12 21:32:05 +0800

    FibonacciSequenceupdate

--- Java/RandomProjects/src/algorithms/FibonacciSequence.java
+++ Java/RandomProjects/src/algorithms/FibonacciSequence.java
@@ -42,6 +42,20 @@ public class FibonacciSequence {
         }
     }
     
+    /*
+    
+    Using Dyanamic Programming, with the help of for loop we simulate the fibonacci
+    by creating an array and then adding the previous number to the latest number
+    */
+    public int fibonacciDynamic(int num) {
+        int[] arr = new int[num + 1];
+        arr[0] = 0;
+        arr[0] = 1;
+        for (int i = 2;i < num;i++) {
+            arr[i] = arr[i - 1] + arr[i - 2];
+        }
+        return arr[num];
+    }
     
     public FibonacciSequence() {
         
