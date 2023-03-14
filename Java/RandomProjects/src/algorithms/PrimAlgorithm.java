7ce6e9388c805b7fe49a28c8adf3aad898b4782f
JowenGonzales <gonzalesmj0942@gmail.com>
2023-03-14 21:34:10 +0800

    PrimAlgorithm

--- Java/RandomProjects/src/algorithms/PrimAlgorithm.java
+++ Java/RandomProjects/src/algorithms/PrimAlgorithm.java
@@ -15,15 +15,15 @@ public class PrimAlgorithm {
     
     public static void main(String[] args) {
         int[][] graph = {
-            {0, 2, 0, 6, 0},
-            {2, 0, 3, 8, 5},
-            {0, 3, 0, 0, 7},
-            {6, 8, 0, 0, 9},
-            {0, 5, 7, 9, 0}  
+                {0, 2, 0, 6, 0},
+                {2, 0, 3, 8, 5},
+                {0, 3, 0, 0, 7},
+                {6, 8, 0, 0, 9},
+                {0, 5, 7, 9, 0}
         };
         int[] parent = primMST(graph);
         System.out.println("Edge   Weight");
-        for (int i = 0; i < graph.length;i++) {
+        for (int i = 1; i < graph.length; i++) {
             System.out.println(parent[i] + " - " + i + "    " + graph[i][parent[i]]);
         }
     }
@@ -31,7 +31,6 @@ public class PrimAlgorithm {
     public static int[] primMST(int[][] graph) {
         int[] parent = new int[graph.length];
         int[] key = new int[graph.length];
-        
         boolean[] mstSet = new boolean[graph.length];
         Arrays.fill(key, Integer.MAX_VALUE);
         Arrays.fill(mstSet, false);
@@ -40,16 +39,25 @@ public class PrimAlgorithm {
         for (int i = 0; i < graph.length - 1; i++) {
             int u = minKey(key, mstSet);
             mstSet[u] = true;
-            for (int v = 0;v < graph.length;v++) {
+            for (int v = 0; v < graph.length; v++) {
                 if (graph[u][v] != 0 && !mstSet[v] && graph[u][v] < key[v]) {
                     parent[v] = u;
-                    key[v] = u;
+                    key[v] = graph[u][v];
                 }
             }
         }
         return parent;
-        
-        
+    }
+    public static int minKey(int[] key, boolean[] mstSet) {
+        int minIndex = -1;
+        int minValue = Integer.MAX_VALUE;
+        for (int i = 0; i < key.length; i++) {
+            if (!mstSet[i] && key[i] < minValue) {
+                minIndex = i;
+                minValue = key[i];
+            }
+        }
+        return minIndex;
     }
     
 }
