2dc3b78b171fda5e9ecaf698c26bdc4707e02476
JowenGonzales <gonzalesmj0942@gmail.com>
2023-03-14 21:22:56 +0800

    PrimAlgorithm
    In computer science, Prim's algorithm is a greedy algorithm that finds a minimum spanning tree for a weighted undirected graph. This means it finds a subset of the edges that forms a tree that includes every vertex, where the total weight of all the edges in the tree is minimized

---
+++ Java/RandomProjects/src/algorithms/PrimAlgorithm.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package algorithms;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author JOWEN
+ */
+public class PrimAlgorithm {
+    
+    public static void main(String[] args) {
+        int[][] graph = {
+            {0, 2, 0, 6, 0},
+            {2, 0, 3, 8, 5},
+            {0, 3, 0, 0, 7},
+            {6, 8, 0, 0, 9},
+            {0, 5, 7, 9, 0}  
+        };
+        int[] parent = primMST(graph);
+        System.out.println("Edge   Weight");
+        for (int i = 0; i < graph.length;i++) {
+            System.out.println(parent[i] + " - " + i + "    " + graph[i][parent[i]]);
+        }
+    }
+    
+    public static int[] primMST(int[][] graph) {
+        int[] parent = new int[graph.length];
+        int[] key = new int[graph.length];
+        
+        boolean[] mstSet = new boolean[graph.length];
+        Arrays.fill(key, Integer.MAX_VALUE);
+        Arrays.fill(mstSet, false);
+        key[0] = 0;
+        parent[0] = -1;
+        for (int i = 0; i < graph.length - 1; i++) {
+            int u = minKey(key, mstSet);
+            mstSet[u] = true;
+            for (int v = 0;v < graph.length;v++) {
+                if (graph[u][v] != 0 && !mstSet[v] && graph[u][v] < key[v]) {
+                    parent[v] = u;
+                    key[v] = u;
+                }
+            }
+        }
+        return parent;
+        
+        
+    }
+    
+}
