cff79ca5b762193e09f45c29228b85a8a7cf9438
JowenGonzales <gonzalesmj0942@gmail.com>
2023-05-01 09:59:49 +0800

    CelciusToFahrenheit

--- Java/RandomProjects/src/CelsiusToFahrenheit.java
+++ Java/RandomProjects/src/CelsiusToFahrenheit.java
@@ -9,5 +9,9 @@
  * @author JOWEN
  */
 public class CelsiusToFahrenheit {
-    
+    public static void main(String[] args) {
+        double degreecelcius = 50;
+        double fahrenheit = (degreecelcius * 9/5) + 32;
+        System.out.println(fahrenheit);
+    }
 }
