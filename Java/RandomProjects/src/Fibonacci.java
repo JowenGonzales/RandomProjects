01435606bb01e172c8137d25a2621a82eadaeed9
JowenGonzales <gonzalesmj0942@gmail.com>
2023-05-05 23:40:45 +0800

    Fibonacci

---
+++ Java/RandomProjects/src/Fibonacci.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author JOWEN
+ */
+public class Fibonacci {
+    public static void main(String[] args) {
+        int num = 100;
+        int fib = fibonacci(num);
+        System.out.println(fib);
+    }
+    public static int fibonacci(int num) {
+        if (num <= 1) {
+            return num;
+        } else {
+            return fibonacci(num - 1) + fibonacci(num - 2);
+        }
+    }
+}
