ea96c26a20c4dfbdf4d4126e40a516e4b60ccf06
JowenGonzales <gonzalesmj0942@gmail.com>
2023-05-05 23:45:34 +0800

    FibonacciMemoization

--- Java/RandomProjects/src/Fibonacci.java
+++ Java/RandomProjects/src/Fibonacci.java
@@ -1,3 +1,7 @@
+
+import java.util.HashMap;
+import java.util.Map;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -10,8 +14,8 @@
  */
 public class Fibonacci {
     public static void main(String[] args) {
-        int num = 100;
-        int fib = fibonacci(num);
+        int num = 10;
+        int fib = fibonnacciIterative(num);
         System.out.println(fib);
     }
     public static int fibonacci(int num) {
@@ -21,4 +25,36 @@ public class Fibonacci {
             return fibonacci(num - 1) + fibonacci(num - 2);
         }
     }
-}
+    public static int fibonnacciIterative(int num) {
+        if (num <= 1) {
+            return num;
+        } 
+        int fib = 1;
+        int prevFib = 1;
+        
+        for (int i = 2;i < num; i++) {
+            int temp = fib;
+            fib += prevFib;
+            prevFib = temp;
+        }
+        return fib;
+    }
+    
+    private static Map<Integer, Integer> memo = new HashMap<>();
+    public static int fibonnacciMemoization(int num) {
+        
+         if (memo.containsKey(num)) {
+            return memo.get(num);
+        }
+
+        int fib;
+        if (num <= 1) {
+            fib = num;
+        } else {
+            fib = fibonacci(num - 1) + fibonacci(num - 2);
+        }
+
+        memo.put(num, fib);
+        return fib;
+    }
+} 
