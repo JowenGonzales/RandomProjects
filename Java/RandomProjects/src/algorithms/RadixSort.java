679d15723b7417127d47af2e27bd3be7b8a2e635
JowenGonzales <gonzalesmj0942@gmail.com>
2023-05-21 23:17:41 +0800

    RadixSort
    Radix Sort is the answer. The idea of Radix Sort is to do digit by digit sort starting from least significant digit to most significant digit.

---
+++ Java/RandomProjects/src/algorithms/RadixSort.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package algorithms;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author JOWEN
+ */
+public class RadixSort {
+    public static void main(String[] args) {
+        int[] arr = { 1, 3, 5, 7, 9, 11, 13, 15};
+        int key = 15;
+        radixSort(arr);
+        
+        System.out.println(Arrays.toString(arr));
+    }
+    
+    public static void radixSort(int[] arr) {
+        if (arr == null || arr.length == 0) {
+            return;
+        }
+
+        int max = getMaxValue(arr);
+
+        for (int exp = 1; max / exp > 0; exp *= 10) {
+            countingSortByDigit(arr, exp);
+        }
+    }
+
+    private static void countingSortByDigit(int[] arr, int exp) {
+        int n = arr.length;
+        int[] output = new int[n];
+        int[] count = new int[10];
+
+        for (int i = 0; i < n; i++) {
+            int digit = (arr[i] / exp) % 10;
+            count[digit]++;
+        }
+
+        
+        for (int i = 1; i < 10; i++) {
+            count[i] += count[i - 1];
+        }
+
+        
+        for (int i = n - 1; i >= 0; i--) {
+            int digit = (arr[i] / exp) % 10;
+            output[count[digit] - 1] = arr[i];
+            count[digit]--;
+        }
+
+        System.arraycopy(output, 0, arr, 0, n);
+    }
+
+    private static int getMaxValue(int[] arr) {
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+}
